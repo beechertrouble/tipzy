a6b5606b2e870295a92b4e17917d75c927c03d2d
beecher <beechertrouble@gmail.com>
2016-07-20 16:15:23 -0400

    added some demo things

--- demo/demo.js
+++ demo/demo.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
 	_tipzy.init();	
 	
 	_tipzy.addTip($("#demo_add_later"), 'I manually added this on docready');
+	_tipzy.addTip($("#demo_add_later2"), 'I manually added this on docready- also as well too');
 
 });
